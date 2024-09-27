8a4c36cc24dd1d7efb18396210a1d928cb624a71
puxxbu <pusswarog@gmail.com>
2024-09-27 11:47:02 +0700

    feature(user) : add user

--- PeerLandingFE/Controllers/api/ApiMstUserController.cs
+++ PeerLandingFE/Controllers/api/ApiMstUserController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PeerLandingFE.DTO.Req;
 using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 
 namespace PeerLandingFE.Controllers.api
 {
@@ -34,7 +37,57 @@ namespace PeerLandingFE.Controllers.api
 
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetUserById(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return BadRequest("Id is required");
+            }
 
+            var token = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
+            var response = await _httpClient.GetAsync($"https://localhost:7239/rest/v1/user/GetUserbyId?id={id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                return Ok(jsonData);
+            }
+            else
+            {
+                return BadRequest("Failed to get user");
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser(string id, [FromBody] ReqMstUserDto reqMstUserDto)
+        {
+            if (reqMstUserDto == null)
+            {
+                return BadRequest("User data is required");
+            }
+
+            var token = Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var json = JsonSerializer.Serialize(reqMstUserDto);
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PutAsync($"https://localhost:7239/rest/v1/user/UpdatebyAdmin?id={id}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                return Ok(jsonData);
+            }
+            else
+            {
+                return BadRequest("Failed to update user");
+            }
+
+        }
     }
 }
--- PeerLandingFE/wwwroot/js/mstuser.js
+++ PeerLandingFE/wwwroot/js/mstuser.js
@@ -1,3 +1,4 @@
+
 async function fetchUsers() {
     const token = localStorage.getItem('token');
     const response = await fetch('ApiMstUser/GetAllUsers', {
@@ -9,7 +10,7 @@ async function fetchUsers() {
 
     if (!response.ok) {
         alert('Failed to fetch users');
-        return; 
+        return;
     }
 
 
@@ -34,8 +35,11 @@ function populateUserTable(users) {
             <td>${user.email}</td>
             <td>${user.role}</td>
             <td>${user.balance}</td>
-            <td><button class="btn btn-primary btn-sm">Edit</button></td>
-            <td><button class="btn btn-danger btn-sm">Delete</button></td>
+            <td>
+            <button class="btn btn-primary btn-sm" onclick="editUser('${user.id}')">Edit</button>
+            <button class="btn btn-danger btn-sm">Delete</button>
+            </td>
+          
         `;
 
         userTableBody.appendChild(row);
@@ -43,3 +47,86 @@ function populateUserTable(users) {
 }
 
 window.onload = fetchUsers();
+
+function editUser(id) {
+    //alert('Edit user with id: ' + id);
+    const token = localStorage.getItem('token');
+
+    console.log('fetching user with id: ' + id);
+
+    fetch('ApiMstUser/GetUserById?id=' + id, {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                alert('Failed to fetch user');
+                return response.json();
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data && data.data) {
+                const user = data.data;
+
+                document.getElementById('userName').value = user.name;
+                document.getElementById('userRole').value = user.role;
+                document.getElementById('userBalance').value = user.balance;
+                document.getElementById('userId').value = user.id;
+
+                $('#editUserModal').modal('show');
+            } else {
+                alert('User not found');
+            }
+        })
+        .catch(error => {
+            alert('An error occured while fetching user: ' + error.message);
+        });
+}
+
+
+
+async function updateUser() {
+    const id = document.getElementById('userId').value;
+    const name = document.getElementById('userName').value;
+    const role = document.getElementById('userRole').value;
+    const balance = document.getElementById('userBalance').value;
+
+    const reqMstUserDto = {
+        name: name,
+        role: role,
+        balance: parseFloat(balance)
+    }
+
+    console.log(reqMstUserDto);
+
+
+    const token = localStorage.getItem('token');
+
+    const response = await fetch('ApiMstUser/UpdateUser?id=' + id, {
+        method: 'PUT',
+        headers: {
+            'Authorization': 'Bearer ' + token,
+            'Content-Type': 'application/json'
+        },
+        body : JSON.stringify(reqMstUserDto)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to update user');
+            }
+            return response.json();
+        })
+        .then(data => {
+            alert('User updated successfully');
+            $('#editUserModal').modal('hide');
+            fetchUsers();
+        })
+        .catch(error => {
+            alert('An error occured while update user: ' + error.message);
+        });
+
+
+}
